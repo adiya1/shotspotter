008bf5564ccb307bf90263c752445878c0b067ec
unknown <you@example.com>
2019-04-22 21:10:31 -0400

    formatted the shiny app and changed the formatting of title and subtitle on the plot

--- Shotspotter/app.R
+++ Shotspotter/app.R
@@ -1,41 +1,27 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
-library(tidyverse)
-library(sf)
-library(tigris)
-library(ggthemes)
+
 
 ui <- fluidPage(
-  titlePanel(h1("San Francisco", style = "font-family: 'times'", align = "center")),
+  titlePanel(h1("Animated Visualization of Data on Shootings in San Francisco", 
+                align = "center")),
   
   sidebarLayout(
                 sidebarPanel(
-                  h3("Source", style = "font-family: 'times'; font-si16pt"),
-                  p("Data from Justice Tech Lab for January 2013 through June 2015. http://justicetechlab.org/shotspotter-data/", style = "font-family: 'times'; font-si16pt")
+                  h3("Source"),
+                  p("Data from Justice Tech Lab for January 2013 through June 2015. http://justicetechlab.org/shotspotter-data/")
                   ),
                 
                 mainPanel(
-                
-                  h2("Animated Map", style = "font-family: 'times'", align = "center"),
                   imageOutput("anim"))
   ))
 
 
 server <- function(input, output) {
     
-    # Create scatterplot object the plotOutput function is expecting
     output$anim <- renderImage(list(src = "sanfran_anim.gif"), 
                                deleteFile = FALSE)
   }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
 
--- script.R
+++ script.R
@@ -23,9 +23,12 @@ ggplot(data = shapes) +
   theme_map() +
   coord_sf(xlim = c(-122.55, -122.3), ylim = c(37.7, 37.85)) +
   transition_time(hour) +
-  labs(title = "Shootings in San Francisco by the Hour Starting from Midnight {frame_time}",
-       subtitle = "Data from January 2013 through June 2015 shows increase in shootings at nighttime",
-       caption = "Source: Justice Tech Lab") 
+  labs(title = "Aggregate Shootings in San Francisco by the Hour \nStarting from Midnight {frame_time}",
+       subtitle = "Data from January 2013 through June 2015 shows dramatic increase \nin shootings towards midnight",
+       caption = "Source: Justice Tech Lab") +
+  theme(plot.title = element_text(size = 20)) +
+  theme(plot.subtitle = element_text(size = 15)) +
+  theme(plot.caption = element_text(size = 10))
   anim_save("Shotspotter/sanfran_anim.gif", animation = last_animation())
 
   
